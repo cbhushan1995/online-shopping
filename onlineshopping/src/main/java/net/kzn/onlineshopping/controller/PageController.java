6fc3237fdbb6cbaf9d500a3d5d0ae3b2b85aa9d8
cbhushan1995 <cbhushan1995@gmail.com>
2017-07-09 19:29:34 +0530

    successfully update UI and linked with backend project !

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,17 +1,29 @@
 
 package net.kzn.onlineshopping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.kzn.shoppingbackend.dao.CategoryDAO;
+import net.kzn.shoppingbackend.dto.Category;
+
 @Controller
 public class PageController {
-
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
+	
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("page");
 		modelAndView.addObject("title", "HOME");
+		
+//		passing the list of categories
+		modelAndView.addObject("categories",categoryDAO.list());
+		
 		modelAndView.addObject("userClickHome", true);
 		return modelAndView;
 	}
@@ -61,4 +73,38 @@ public class PageController {
 		modelAndView.addObject("userClickContact", true);
 		return modelAndView;
 	}
+	
+//	methods to load all the products and based on category
+	@RequestMapping(value = "/show/all/products")
+	public ModelAndView showAllProducts() {
+		ModelAndView modelAndView = new ModelAndView("page");
+		modelAndView.addObject("title", "All Products");
+		
+//		passing the list of categories
+		modelAndView.addObject("categories",categoryDAO.list());
+		
+		modelAndView.addObject("userClickAllProducts", true);
+		return modelAndView;
+	}
+
+
+
+@RequestMapping(value = "/show/category/{id}/products")
+public ModelAndView showCategoryProducts(@PathVariable("id") int id) {
+	ModelAndView modelAndView = new ModelAndView("page");
+	
+	//categoryDAO to fetch a single category 
+	Category category = null;
+	category = categoryDAO.get(id);
+	
+	modelAndView.addObject("title", category.getName());
+	
+//	passing the list of categories
+	modelAndView.addObject("categories",categoryDAO.list());
+	//passing the single category object
+	modelAndView.addObject("category",category);
+	modelAndView.addObject("userClickCategoryProducts", true);
+	return modelAndView;
+}
+
 }
--- onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
+++ onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
@@ -67,6 +67,14 @@
 			<c:if test="${userClickContact == true }">
 				<%@include file="contact.jsp"%>
 			</c:if>
+			
+			
+			
+			<!-- 	only when user clicks Contact -->
+			<c:if test="${userClickAllProducts == true or userClickCategoryProducts == true}">
+				<%@include file="listProducts.jsp"%>
+			</c:if>
+			
 		</div>
 
 		<!-- Footer comes here-->
